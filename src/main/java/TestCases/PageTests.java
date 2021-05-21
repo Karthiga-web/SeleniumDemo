79dc03e019533cf82fd3ea550d1e4ea2c9da3ad4
Karthiga-web <kar07091995@gmail.com>
2021-05-21 10:19:02 -0700

    Changing to Framework Model

--- src/main/java/TestCases/PageTests.java
+++ src/main/java/TestCases/PageTests.java
@@ -1,11 +1,17 @@
 package TestCases;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import frameworkModel.HomePage;
@@ -19,27 +25,57 @@ public class PageTests {
 				"C:\\Users\\grkar\\OneDrive\\Documents\\Selenium\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebDriverWait wait = new WebDriverWait(driver, 6);
 		driver.navigate().to("https://www.amazon.com/");
 		driver.manage().window().fullscreen();
 		
-		HomePage homepage = new HomePage(driver);
-		Actions a = new Actions(driver);
-		a.moveToElement(homepage.signinClick()).build().perform();
-		homepage.signin().click();
+		homepage(driver);
+		loginpage(driver);
+		searchShampooPage(driver,wait);
 		
-		LoginPage loginpage = new LoginPage(driver);
-		loginpage.email().sendKeys("grkarthikagr@yahoo.com");;
-		loginpage.continueClick().click();;
-		loginpage.password().sendKeys("123456789");
-		loginpage.submitClick().click();
-		
+		driver.close();
+	}
+
+	private void filterSetting(WebDriver driver, SearchShampooPage searchShampooPage, WebDriverWait wait) {
+		searchShampooPage.filter1().click();
+		wait.until(ExpectedConditions.visibilityOf(searchShampooPage.filter2()));
+		Assert.assertTrue(searchShampooPage.filter2().isEnabled());
+		wait.until(ExpectedConditions.visibilityOf(searchShampooPage.filter3()));
+		searchShampooPage.filter3().click();
+		wait.until(ExpectedConditions.visibilityOf(searchShampooPage.filter3()));
+		Assert.assertTrue(searchShampooPage.filter3().isEnabled());
+
+		List<WebElement> hairFilter = searchShampooPage.filter4();
+		hairFilter.iterator().forEachRemaining(item -> {
+			if (item.getText().toLowerCase().equalsIgnoreCase("dry")) {
+				item.click();
+				Assert.assertTrue(item.isEnabled());
+			}
+		});
+	}
+
+	private void searchShampooPage(WebDriver driver, WebDriverWait wait) {
 		SearchShampooPage searchShampooPage = new SearchShampooPage(driver);
 		searchShampooPage.searchKeyword().sendKeys("Pantene Shampoo");
 		searchShampooPage.serachButtonClick().click();
 		driver.navigate().back();
 		searchShampooPage.searchKeyword().sendKeys("Pantene Shampoo");
 		searchShampooPage.searchKeyword().sendKeys(Keys.ENTER);
-		
-		driver.close();
+		filterSetting(driver,searchShampooPage,wait);
+	}
+
+	private void loginpage(WebDriver driver) {
+		LoginPage loginpage = new LoginPage(driver);
+		loginpage.email().sendKeys("grkarthikagr@yahoo.com");;
+		loginpage.continueClick().click();;
+		loginpage.password().sendKeys("123456789");
+		loginpage.submitClick().click();
+	}
+
+	private void homepage(WebDriver driver) {
+		HomePage homepage = new HomePage(driver);
+		Actions a = new Actions(driver);
+		a.moveToElement(homepage.signinClick()).build().perform();
+		homepage.signin().click();
 	}
 }
--- src/main/java/frameworkModel/SearchShampooPage.java
+++ src/main/java/frameworkModel/SearchShampooPage.java
@@ -1,5 +1,7 @@
 package frameworkModel;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,14 +18,42 @@ public class SearchShampooPage {
 	@FindBy(id="twotabsearchtextbox")
 	WebElement searchKeyword ;
 	
-	@FindBy(id="nav-search-submit-button")
-	WebElement serachButtonClick ;
-	
 	public WebElement searchKeyword() {
 		return searchKeyword;
 	}
 	
+	@FindBy(id="nav-search-submit-button")
+	WebElement serachButtonClick ;
+	
 	public WebElement serachButtonClick() {
 		return serachButtonClick;
+	}
+	
+	@FindBy(xpath="//*[@id='p_76/1249134011']/descendant::i")
+	WebElement filter1 ;
+	
+	public WebElement filter1() {
+		return filter1;
+	}
+	
+	@FindBy(id="//*[@id='p_76/1249134011']/descendant::input")
+	WebElement filter2 ;
+	
+	public WebElement filter2() {
+		return filter2;
+	}
+	
+	@FindBy(id="//*[@id='p_72/1248876011']/descendant::section")
+	WebElement filter3 ;
+	
+	public WebElement filter3() {
+		return filter3;
+	}
+	
+	@FindBy(id="//div[@id='filters']/child::ul[1]/li")
+	List<WebElement> filter4 ;
+	
+	public List<WebElement> filter4() {
+		return filter4;
 	}
 }
